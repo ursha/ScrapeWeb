57b57db9c9870408e63c1c1242d9045a4b88f317
Ursha <benas.pekarskis@gmail.com>
2022-02-02 14:41:57 +0000

    Initial

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from selenium import webdriver
-#Set options to make browser easier
-options=webdriver.ChromeOptions()
-options.add_argument("disable-infobars")
-options.add_argument("start-maximized")
-options.add_argument("disable-dev-shm-usage")
-options.add_argument("no-sandbox")
-options.add_experimental_option("excludeSwitches",["enable-automation"])
-options.add_argument("disable-blink-features=AutomationControlled")
-driver =webdriver.Chrome()
+  from selenium import webdriver
+
+def get_driver():  
+  #Set options to make browser easier
+  options=webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  
+  driver =webdriver.Chrome(options)
+  driver.get("https://automated.pythonanywhere.com/")
+  return driver
+
+
+
