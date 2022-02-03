329b3d7b13cbf6649ea60b514a56e7f1cff39786
Ursha <benas.pekarskis@gmail.com>
2022-02-03 20:50:12 +0000

    full

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
-def get_driver():  
-  #Set options to make browser easier
-  options=webdriver.ChromeOptions()
+
+def get_drvier():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
   options.add_argument("start-maximized")
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
-  
-  driver =webdriver.Chrome(options=options)
-  driver.get("https://automated.pythonanywhere.com/")
+
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
+
 def clean_text(text):
-  output=float(text.split(': ')[1])
+  """Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
   return output
 
-
 def main():
-  driver=get_driver()
+  driver = get_drvier()
+
+  # Find and fill in username and password 
+  driver.find_element(by="id", value="id_username").send_keys("automated")
   time.sleep(2)
-  element=driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
 
-print(main())  
+  # Click on Home link and wait 2 sec
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  time.sleep(2)
 
+  # Scrape the temperature value
+  text = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+  return clean_text(text)
+
+print(main())
